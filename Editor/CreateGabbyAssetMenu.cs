fd447008b4ec92e99499d8ddcf7bfb4265c29cb0
Zack Dawson <zdawson@users.noreply.github.com>
2021-10-25 23:10:00 -0400

    Add menu option to create a new gabby script

---
+++ Editor/CreateGabbyAssetMenu.cs
@@ -0,0 +1,59 @@
+using System;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateGabbyAssetMenu
+{
+     [MenuItem("Assets/Create/Gabby Script", false, 2000)]
+    private static void CreateGabbyScriptAsset(MenuCommand command)
+    {
+        // Create a new Gabby script
+        string directory = "Assets/";
+        if (Selection.activeObject != null)
+        {
+            directory = AssetDatabase.GetAssetPath(Selection.activeObject);
+            if (directory.Length == 0)
+            {
+                return;
+            }
+
+            // If the selected item is not a directory, get the containing folder
+            FileAttributes attr = File.GetAttributes(directory);
+            if (!attr.HasFlag(FileAttributes.Directory))
+            {
+                directory = Path.GetDirectoryName(directory);
+            }
+        }
+        
+        string filePath = Path.Combine(directory, "New Gabby Script.gab");
+        if (File.Exists(filePath))
+        {
+            filePath = Path.Combine(directory, $"New Gabby Script ({DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}).gab");
+            if (File.Exists(filePath))
+            {
+                Debug.LogError("Unable to create file - file exists.");
+                return;
+            }
+        }
+
+        // Create the file
+        string[] fileContents =
+        {
+            "gabby 0.2",
+            "language english",
+            "",
+            "// Write your dialogue here"
+        };
+
+        StringBuilder sb = new StringBuilder();
+        foreach (string line in fileContents)
+        {
+            sb.Append(line);
+            sb.Append("\n");
+        }
+
+        ProjectWindowUtil.CreateAssetWithContent(filePath, sb.ToString());
+    }
+}
